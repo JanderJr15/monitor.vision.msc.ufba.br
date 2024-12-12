e595a25190bb2288aaeb93baedb4c3fb9535b19c
janderjr15 <jandersantos@ufba.br>
2024-12-12 09:04:46 -0300

    feat: refactoring monitor module
    - refactoring functionality to collect machine ip address in get_ip_address.py.

--- monitor/get_ip_address.py
+++ monitor/get_ip_address.py
@@ -1,13 +1,34 @@
 import socket
+from typing import Union
 
-def get_local_ip():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        try:
-            # Conecta a um endereço remoto para determinar o IP local
-            s.connect(("8.8.8.8", 80))  # Usa o Google DNS como referência
-            return s.getsockname()[0]
-        except Exception as e:
-            return f"Erro ao obter IP: {e}"
+class NetworkUtils:
+    """
+    Classe utilitária para operações relacionadas à rede.
+    """
 
-ip_address = get_local_ip()
-print(f"Endereço IP Local: {ip_address}")
+    @staticmethod
+    def get_local_ip() -> Union[str, None]:
+        """
+        Obtém o endereço IP local da máquina.
+
+        Retorna:
+            str: O endereço IP local.
+            None: Se não for possível determinar o endereço IP.
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            try:
+                # Conecta a um endereço remoto para determinar o IP local
+                s.connect(("8.8.8.8", 80))
+                return s.getsockname()[0]
+            except (OSError, socket.error) as e:
+                # Log adequado (caso necessário, utilizando biblioteca de logging)
+                print(f"Erro ao obter IP local: {e}")
+                return None
+
+# Exemplo de uso
+if __name__ == "__main__":
+    ip_address = NetworkUtils.get_local_ip()
+    if ip_address:
+        print(f"Endereço IP Local: {ip_address}")
+    else:
+        print("Não foi possível determinar o endereço IP local.")
